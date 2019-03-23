cbb5b8b92c1b38ecf1c723c03f5fda2c45428201
AGE <age.esquivel@gmail.com>
2019-03-23 18:21:10 -0500

    code to find your friend

--- FriendFinder/routing/apiRoutes.js
+++ FriendFinder/routing/apiRoutes.js
@@ -3,15 +3,12 @@
 // Bring friends into the file
 // ==============================================================================
 const friends = require("../app/data/friends")
-
 // ===============================================================================
 // ROUTING
 // ===============================================================================
-
 //allowsfor export of this function
 //in this app we will export to server.js
 module.exports = function (app) {
-
     // ---------------------------------------------------------------------------
     // API GET Requests
     // Below code handles when users "visit" a page.
@@ -21,7 +18,6 @@ module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friends)
     })
-
     // ---------------------------------------------------------------------------
     // API POST Requests
     // Below code handles when a user submits a form and thus submits data to the server.
@@ -31,16 +27,51 @@ module.exports = function (app) {
     // Then the server saves the data to the friends array)
     // ---------------------------------------------------------------------------
     app.post("/api/friends", function (req, res) {
+
+
+        //we will manipulate this array after sorting is complete
+        const friendArr = []
+
+        //let's reduce our score to do math
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        //reduce client score first
+        let surveyNum = parseInt(req.body.scores.reduce(reducer))
+        
+        //this wil allow us to loop through the array of objects 
+        for (friend in friends) {
+            //reducing to scores in this piece of code
+            let num = parseInt(friends[friend].scores.reduce(reducer));
+
+
+            console.log(`${friends[friend].name} , ${num}`);
+            
+            //now we push into a new array to play with..
+            //..oh and there is some math for the score
+            friendArr.push({
+                friend: friends[friend].name,
+                image: friends[friend].photo,
+                friendScore: Math.abs(num - surveyNum)
+            })
+        }
+
+//this one will sort our new list in ascending order bassed on scores
+        let friendsScoreSorted = friendArr.sort(function (a, b) { return a.friendScore - b.friendScore })
+
+        console.log(friendsScoreSorted[0]);
+//add new friend to list
         friends.push(req.body);
-        friends
 
 
         //return data back to client
-        res.json(   );
+        //index 0 should be the best match
+        res.json(
+            {
+              name: friendsScoreSorted[0].friend,
+              photo: friendsScoreSorted[0].image
+                      }
+          );
     })
 }
-
-
 
 
 
